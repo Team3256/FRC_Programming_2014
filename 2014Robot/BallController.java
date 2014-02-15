bbacfd5e8d924bd8ecf4046d5e29adf4b7a98dfa
vcrobotics <aloha1epp@yahoo.com>
2014-02-14 19:51:58 -0800

    ball control class finished

---
+++ 2014Robot/BallController.java
@@ -0,0 +1,49 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Talon;
+
+//import edu.wpi.first.wpilibj.DigitalSource;
+/**
+ *
+ * @author VCS Robotics
+ */
+public class BallController {
+    private DigitalInput photoResistor;
+    private DigitalInput limitSwitch;
+    private Solenoid catcherArm;
+    private Talon armRoller;
+    private boolean photoReceived;
+    BallController(int photoPort, int limitPort, int catcherArm, int rollerMotor){
+        photoResistor = new DigitalInput(photoPort);
+        limitSwitch = new DigitalInput(limitPort);
+        this.catcherArm = new Solenoid(catcherArm);
+        armRoller = new Talon(rollerMotor);
+    }
+    
+    public boolean receivedBall(){
+        
+        return  !photoResistor.get() && limitSwitch.get();
+    }
+    
+    public void catchFromAir(){
+        if(receivedBall() )
+            catcherArm.set(true);
+        else
+            catcherArm.set(false);
+            
+    }
+    
+    public void pickFromGround(){
+       if(!receivedBall()){
+           armRoller.set(1);
+       }
+    }
+    
+    
+}
+
